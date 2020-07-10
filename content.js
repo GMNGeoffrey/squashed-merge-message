177c2bdc33918d10a08fb5e3d895ae391e68720b
Geoffrey Martin-Noble <gcmn@google.com>
2020-07-10 11:36:10 -0700

    Add logging instead of silently failing

--- content.js
+++ content.js
@@ -1,18 +1,34 @@
 function copyPrDescription() {
+  console.log("squashed-merge-message: Triggered copyPrDescription");
   const prTitleEl = document.getElementById("issue_title");
-  if (!prTitleEl) return;
+  if (!prTitleEl) {
+    console.warn("squashed-merge-message: Failed to find PR title!");
+    return;
+  }
 
   const prNumberEl = document.querySelector(".gh-header-title .gh-header-number");
-  if (!prNumberEl) return;
+  if (!prNumberEl) {
+    console.warn("squashed-merge-message: Failed to find PR Number");
+    return;
+  }
 
   const prBodyEl = document.querySelector('.comment-form-textarea[name="pull_request[body]"]');
-  if (!prBodyEl) return;
+  if (!prBodyEl) {
+    console.warn("squashed-merge-message: Failed to find PR Body");
+    return;
+  }
 
   const titleField = document.getElementById('merge_title_field');
-  if (!titleField) return;
+  if (!titleField) {
+    console.warn("squashed-merge-message: Failed to find merge commit title");
+    return;
+  }
 
   const messageField = document.getElementById('merge_message_field');
-  if (!messageField) return;
+  if (!messageField) {
+    console.warn("squashed-merge-message: Failed to find merge commit message");
+    return;
+  }
 
   const commitTitle = `${prTitleEl.value} (${prNumberEl.textContent})`;
   const commitBody = prBodyEl.textContent;
@@ -22,16 +38,20 @@ function copyPrDescription() {
 }
 
 function addMergeListeners() {
+  console.log("squashed-merge-message: adding listener");
   const squashButton = document.querySelector('.merge-message .btn-group-squash');
   const mergeButton = document.querySelector('.merge-message .btn-group-merge');
 
   if (squashButton) {
     squashButton.addEventListener('click', copyPrDescription);
+  } else {
+    console.warn("squashed-merge-message: Couldn't find squash button");
   }
   if (mergeButton) {
     mergeButton.addEventListener('click', copyPrDescription);
+  } else {
+    console.warn("squashed-merge-message: couldn't find merge button");
   }
-
 }
 
 document.addEventListener('pjax:end', addMergeListeners);
